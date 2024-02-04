6918c0dffd10584e05b253a1eaec9de04086f00a
Mingjia Wang <mingjiawangdev@gmail.com>
2024-02-04 11:46:09 -0800

    Finished setting up top panel

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -3,12 +3,20 @@ import './Home.css';
 
 function App() {
   return (
-    <>
+    <div className='page'>
       <div className="App">
         <h1>AI DO</h1>
+        <p className='tagline'>Crafting Dream Weddings with AI Precision</p>
+        <p className='description'>Experience stress-free wedding planning with AI Do! Our AI-driven chat platform curates personalized, budget-friendly weddings, seamlessly pairing you with the perfect vendors</p>
+        <div className='button-container'>
+          <button className='gs-button'>Get Started</button>
+          <button className='lm-button'>Learn More</button>
+        </div>
       </div>
-      <div className='form'></div>
-    </>
+      <div className='form'>
+
+      </div>
+    </div>
     
   );
 }
