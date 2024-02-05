72479f46eb62ab5cef74d6a4b4e96bee29c7b5d3
Mingjia Wang <mingjiawangdev@gmail.com>
2024-02-05 10:12:46 -0800

    Got loading spinner working

--- app/src/pages/Match.jsx
+++ app/src/pages/Match.jsx
@@ -1,12 +1,38 @@
+import { useState, useEffect } from 'react';
+import { ring } from 'ldrs';
 import './Match.css';
 
 function Match() {
+    
+    const [ isLoading, setIsLoading ] = useState(true);
+    const options = {
+        
+    };
+
+    const registerRing = () => {
+        ring.register('loading-icon');
+    };
+
+    const findVenues = () => {
+
+    }
+
+    useEffect(() => {
+        registerRing();
+        
+        findVenues();
+        setIsLoading(false);
+    }, []);
+
   return (
     <>
-      <div className="Match">
+      <div className="match-page">
         <h1>Match Page</h1>
+        <div aria-live="polite" aria-busy={isLoading}>
+            {isLoading && <loading-icon></loading-icon>}
+        </div>
       </div>
-      <div className='match-form'></div>
+      {/* <div className='match-form'></div> */}
     </>
     
   );
