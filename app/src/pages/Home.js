88e2f3c89533b8302392c460678f9ce1d210b736
Mingjia Wang <mingjiawangdev@gmail.com>
2024-02-04 17:36:35 -0800

    Got scrollIntoView working to show form

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,7 +1,16 @@
 // import logo from './logo.svg';
+import { useRef } from 'react';
 import './Home.css';
 
 function App() {
+  const formRef = useRef();
+
+  const showForm = () => {
+    formRef.current.scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+
   return (
     <div className='page'>
       <div className="App">
@@ -9,12 +18,29 @@ function App() {
         <p className='tagline'>Crafting Dream Weddings with AI Precision</p>
         <p className='description'>Experience stress-free wedding planning with AI Do! Our AI-driven chat platform curates personalized, budget-friendly weddings, seamlessly pairing you with the perfect vendors</p>
         <div className='button-container'>
-          <button className='gs-button'>Get Started</button>
-          <button className='lm-button'>Learn More</button>
+          <button className='gs-button' onClick={showForm}>Get Started</button>
         </div>
       </div>
-      <div className='form'>
-
+      <div className='form' id='form' ref={formRef}>
+        <div className='form-container'>
+          <div className='input-box'>
+            <p></p>
+            <input></input>
+          </div>
+          <div className='input-box'>
+            <p></p>
+            <input></input>
+          </div>
+          <div className='input-box'>
+            <p></p>
+            <input></input>
+          </div>
+          <div className='input-box'>
+            <p></p>
+            <input></input>
+          </div>
+        </div>
+        
       </div>
     </div>
     
