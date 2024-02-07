472a8066a64a8afdc5d709a92a2d93d782d6c235
Mingjia Wang <mingjiawangdev@gmail.com>
2024-02-06 18:28:14 -0800

    Restored state of app to version from Monday night

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -31,6 +31,35 @@ function App() {
           <button className='gs-button' onClick={showForm}>Get Started</button>
         </div>
       </div>
+      <div className='form' id='form' ref={formRef}>
+        <div className='form-panel'>
+          <div className='form-container'>
+            <div className='input-box'>
+              <h6 className='q-label'>Theme</h6>
+              <p className='question'>What's your dream wedding theme?</p>
+              <input type='text' onChange={(e) => {setTheme(e.target.value)}} placeholder='blank = any'></input>
+            </div>
+            <div className='input-box'>
+            <h6 className='q-label'>Number of Guests</h6>
+              <p className='question'>How many people are you inviting?</p>
+              <input type='text' onRateChange={(e) => {setCapacity(e.target.value)}} placeholder='blank = any'></input>
+            </div>
+            <div className='input-box'>
+              <h6 className='q-label'>Location</h6>
+              <p className='question'>Where in California?</p>
+              <input type='text' onChange={(e) => {setLocation(e.target.value)}} placeholder='blank = any'></input>
+            </div>
+            <div className='input-box'>
+              <h6 className='q-label'>Budget</h6>
+              <p className='question'>What's your price range?</p>
+              <input type='text' onChange={(e) => setBudget(e.target.value)} placeholder='blank = any'></input>
+            </div>
+          </div>
+          <div className='button-div'>
+            <Link to={generateMatchURL()}><button id='submit'>Let's go!</button></Link>
+          </div>
+        </div>
+      </div>
     </div>
     
   );
